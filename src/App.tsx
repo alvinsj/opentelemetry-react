ab5eed782715f84f851336ce3136423953d8510e
Alvin Ng <243186+alvinsj@users.noreply.github.com>
2022-09-14 16:41:35 +0800

    Set up instrumentation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import {
+  ConsoleSpanExporter,
+  BatchSpanProcessor,
+  SimpleSpanProcessor
+} from "@opentelemetry/sdk-trace-base";
+import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
+import { ZoneContextManager } from "@opentelemetry/context-zone";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { Resource } from "@opentelemetry/resources";
+
+const provider = new WebTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "otlp-react"
+  })
+});
+provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+provider.addSpanProcessor(
+  new SimpleSpanProcessor(
+    new OTLPTraceExporter({
+      headers: {}
+    })
+  )
+);
+
+provider.register({
+  // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
+  contextManager: new ZoneContextManager()
+});
+
+// Registering instrumentations
+registerInstrumentations({
+  instrumentations: [getWebAutoInstrumentations()]
+});
+
 function App() {
   const [count, setCount] = useState(0)
 
